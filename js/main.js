637aa312c8a46e2c835d02127cb196fe2284e388
Aaron Paterson <juniha.aaron@gmail.com>
2019-04-09 18:45:03 -0400

    working on projects page

--- js/main.js
+++ js/main.js
@@ -18,5 +18,26 @@
 	// button is touched event, then open the menu and close it
 	button.addEventListener("click", hamburgerMenu, false);
 
+	// Donate modal 
+
+
+
+	// console.log("Linked up");
+
+	//console.log("Hello " + firstName);
+	console.log("Donate now is working ");
+
+	// get the button with the id call button
+	document.getElementById('donateBut').addEventListener("click", function() {
+	document.querySelector('.bgModal').style.display = "flex";
+	});
+
+	// create the close function
+	document.querySelector('.close').addEventListener("click", function() {
+	// this is the function here
+		document.querySelector('.bgModal').style.display = "none";
+
+	});
+
 
 })();
