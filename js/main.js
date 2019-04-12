0c95df5e5b8d5b50250184c05abfb77e04297d1c
Aaron Paterson <juniha.aaron@gmail.com>
2019-04-12 14:36:00 -0400

    amended details

--- js/main.js
+++ js/main.js
@@ -2,15 +2,7 @@
 
 (function() {
 
-	// let section1Btn = documentElementById("#section1Btn");
-
-	// // scroll to the page
-	// section1Btn.onclick = function() {
-	// TweenMax.to(window, 1, {scrollTo:{y:".fundingHero", offsetY:50, autoKill:false},
-	//  	ease:Power2.easeOut});
-	// 	console.log('scroll to something');
-	// };
-
+	
 	// hamburger function 
 	"use strict"
 	console.log("Linked up");
