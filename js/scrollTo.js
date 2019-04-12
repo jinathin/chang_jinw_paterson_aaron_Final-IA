733298f0768d09cabb7eaadd287893890de2ed89
Aaron Paterson <juniha.aaron@gmail.com>
2019-04-12 17:01:44 -0400

    squash bugs and sorting the greensock smoothscrooling

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -1,15 +1,31 @@
 
 // Greensock ScrollTo
 
-(function() {
+(function(){
+	"use strict";
 
-let section1Btn = documentElementById("#section1Btn");
+	// collection fo the button for tor for loop
+	let links = document.querySelectorAll(".nav-dropdown a");
+	console.log(links);
 
-	// scroll to the page
-	section1Btn.onclick = function() {
-	TweenMax.to(window, 1, {scrollTo:{y:".fundingHero", offsetY:50, autoKill:false},
-	 	ease:Power2.easeOut});
-		console.log('scroll to something');
-	};
+	// event handler
+	function scrollLink(e) {
+		console.log(e.currentTarget.id.slice(0,-3));
+
+		e.preventDefault();
+		let targetArea = e.currentTarget.id.slice(0,-3);
+		TweenLite.to(window, 1, (scrollTo:{y:`#${targetArea}`, offsetY:70, autoKill:false}});
+	}
+
+	// for loop setting your count to zero aith 'i'
+	for(var i=0; i < links.length; i++) {
+		// add the event listeners to the links[0], [1], [2]
+		links[i].addEventListener("click", scrollLink);
+	}
 
 })();
+
+
+// By default, the ScrollToPlugin will automatically sense if the scroll position was changed outside of itself 
+// (like if the user manually started dragging the scrollbar mid-tween) and cancel that portion of the tween.
+// If, however, you'd like to prevent the auto-killing behavior, set autoKill:false
